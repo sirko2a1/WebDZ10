99862441f1293a06316b6f74568ee513db145ab5
sirko2a1 <134325906+sirko2a1@users.noreply.github.com>
2024-01-02 20:54:15 +0200

    commit нервовий зрив номер 1

--- quo/qou/urls.py
+++ quo/qou/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('registration/', views.registration, name='registration'),
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.logout, name='logout'),
 ]
--- quo/qou/views.py
+++ quo/qou/views.py
@@ -1,15 +1,36 @@
-# myapp/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
-from pymongo import MongoClient
+from django.contrib import messages
 from .models import User
-from quo.settings import db  
+from quo.settings import db
+from django.shortcuts import render
 
-def update_user_list():
-    users_collection = db['users']
-    users_collection.delete_many({}) 
-    for user in User.objects.all():
-        users_collection.insert_one({'username': user.username, 'email': user.email})
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration.html', {'form': form})
 
 @login_required
 def home(request):
@@ -17,3 +38,14 @@ def home(request):
     users_collection = db['users']
     users = list(users_collection.find())
     return render(request, 'home.html', {'users': users})
+
+def update_user_list():
+    users_collection = db['users']
+    users_collection.delete_many({})
+    for user in User.objects.all():
+        users_collection.insert_one({'username': user.username, 'email': user.email})
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
